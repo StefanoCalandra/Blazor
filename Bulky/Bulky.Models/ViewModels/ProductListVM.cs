732f7703bff2a8cbc6fcba28c5cec420aacd6e3f
Stefano Calandra <76255813+StefanoCalandra@users.noreply.github.com>
2025-10-29 15:58:01 +0100

    Add files via upload

---
+++ Bulky/Bulky.Models/ViewModels/ProductListVM.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models.ViewModels
+{
+    public class ProductListVM
+    {
+        [ValidateNever]
+        public IEnumerable<SelectListItem> CategoryList { get; set; } = new List<SelectListItem>();
+
+        [Display(Name = "Category")]
+        public int? SelectedCategoryId { get; set; }
+
+        [Display(Name = "Min price")]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be greater than or equal to zero.")]
+        public double? MinPrice { get; set; }
+
+        [Display(Name = "Max price")]
+        [Range(0, double.MaxValue, ErrorMessage = "Price must be greater than or equal to zero.")]
+        public double? MaxPrice { get; set; }
+
+        [Display(Name = "Search")] 
+        public string? SearchTerm { get; set; }
+    }
+}
